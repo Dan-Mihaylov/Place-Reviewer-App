1451386f8ca46aec2cc4035fd02822a1cb29e865
Daniel Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2024-01-31 00:14:27 +0000

    Improvements and Restructuring
    Restructuring the Templates to be using Template inheritance

--- TheReviewApp/settings.py
+++ TheReviewApp/settings.py
@@ -121,13 +121,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
